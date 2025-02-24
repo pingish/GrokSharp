ceb876e7e220d964d5001853b250dd1be905c067
Oliver Yu <oly@zymergi.com>
2025-02-24 14:49:46 -0800

    Passing tests on more ChatResponse properties

--- Test/ChatResponseTests.cs
+++ Test/ChatResponseTests.cs
@@ -13,27 +13,39 @@ namespace Test
         }
 
         [TestCase("chatResponse.00.json", "chat.completion")]
-        public void Object(string f, string expected)
-        {
-            AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Object);
-        }
+        public void Object(string f, string expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Object);
 
         [TestCase("chatResponse.00.json", "c6f2d009-77ca-40d9-9de5-6d19716e1b4d")]
-        public void Id(string f, string expected)
-        {
-            AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Id);
-        }
+        public void Id(string f, string expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Id);
+        
 
         [TestCase("chatResponse.00.json", 1728646283)]
-        public void CreatedAt(string f, long expected)
-        {
-            AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Created);
-        }
+        public void CreatedAt(string f, long expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Created);
+
+
+        [TestCase("chatResponse.00.json", 15)]
+        public void UsagePromptTokens(string f, long expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Usage?.PromptTokens);
+
+        [TestCase("chatResponse.00.json", 10)]
+        public void UsageCompletionTokens(string f, long expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Usage?.CompletionTokens);
+
+        [TestCase("chatResponse.00.json", 25)]
+        public void UsageTotalTokens(string f, long expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Usage?.TotalTokens);
+
+        [TestCase("chatResponse.00.json", 15)]
+        public void UsageDetailsTextTokens(string f, long expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Usage?.PromptTokensDetails?.TextTokens);
+
+        [TestCase("chatResponse.00.json", 2)]
+        public void UsageDetailsAudioTokens(string f, long expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Usage?.PromptTokensDetails?.AudioTokens);
+
+        [TestCase("chatResponse.00.json", 3)]
+        public void UsageDetailsImageTokens(string f, long expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Usage?.PromptTokensDetails?.ImageTokens);
+
+        [TestCase("chatResponse.00.json", 4)]
+        public void UsageDetailsCachedTokens(string f, long expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.Usage?.PromptTokensDetails?.CachedTokens);
+
 
         [TestCase("chatResponse.00.json", "fp_9877325691")]
-        public void SystemFingerprint(string f, string expected)
-        {
-            AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.SystemFingerprint);
-        }
+        public void SystemFingerprint(string f, string expected) => AssertAreEqual<ChatResponse>(f, expected, chatResponse => chatResponse?.SystemFingerprint);
     }
 }
