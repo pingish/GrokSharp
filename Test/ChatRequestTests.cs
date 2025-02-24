49aeed9d3280a311de086fa310e2745789ef7633
Oliver Yu <oly@zymergi.com>
2025-02-24 12:11:53 -0800

    Testing folder is set up correctly

--- Test/ChatRequestTests.cs
+++ Test/ChatRequestTests.cs
@@ -1,6 +1,18 @@
-﻿
+﻿using NUnit.Framework;
 
 namespace Test
 {
-  
+    [TestFixture]
+    public class ChatRequestTests
+    {
+
+        [TestCase("chatRequest.00.json", "c6f2d009-77ca-40d9-9de5-6d19716e1b4d")]
+        public void Id(string f, string expected)
+        {
+            string actual = "";
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+
+    }  
 }
